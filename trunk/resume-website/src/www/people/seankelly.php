6f91ef86e4d6c7d252c8316913c215d4adef69e6
seankelly <seankelly@58bf4eea-5411-0410-84c9-993ea22221af>
2002-11-27 12:54:08 +0000

    Add my own bio.
    git-svn-id: https://xmlresume.svn.sourceforge.net/svnroot/xmlresume@520 58bf4eea-5411-0410-84c9-993ea22221af

---
+++ trunk/resume-website/src/www/people/seankelly.php
@@ -0,0 +1,45 @@
+<?php
+$pagetitle = "Sean Kelly";
+include_once("../private/header.php");
+?>
+
+<h2>Sean Kelly</h2>
+
+<h5>Founder </h5>
+<h5>Nickname: seankelly </h5>
+<h5>R&eacute;sum&eacute;: 
+  <a href="http://seankelly.biz/modules.php?name=Resume"
+    >http://seankelly.biz/modules.php?name=Resume</a>
+</h5>
+<h5>Wishlist: 
+  <a href="http://www.amazon.com/exec/obidos/registry/wishlist/102-0064359-7166570"
+    >Amazon</a><br>
+  <a href="http://www.thinkgeek.com/brain/gimme.cgi?wid=81cea345e"
+    >ThinkGeek</a>
+</h5>
+
+<p>I founded this project after realizing that no version of my r&eacute;sum&eacute;
+was ever in sync, and there had to be a better way.  At the time, I was also courting
+employers for XML and XSL-related jobs, so what better way to demonstrate such skills
+than by making my entire r&eacute;sum&eacute; an XML and XSL-based system?</p>
+
+<p>Although the initial results were rough around the edges, it worked.  (I'm completely self-employed now.)</p>
+
+<p>But I still wanted it to look better and better.  And that's when it hit me.  There
+must be hundreds of geek-minded people out there who would like to single-source their
+r&eacute;sum&eacute;s ... why not take advantage of that great mass of unemployed
+skill?</p>
+
+<p>By making the XML R&eacute;sum&eacute; Library an open source project, dozens
+of jobless individuals from around the world continually contribute to make the
+system better and better.</p>
+
+<p>Thank you for taking the time to learn more about me, and even if you're already employed,
+please contribute to the project by suggesting features, reporting bugs, submitting
+patches, or shopping for items on the members' wishlists.</p>
+
+<p><a href="index.html">Back to Developer List</a></p>
+
+<?php
+include_once("../private/footer.php");
+?>
