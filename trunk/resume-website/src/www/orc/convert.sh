8f1e89b67d2ef082f498d61bf166eaf5b035c8f4
brandondoyle <brandondoyle@58bf4eea-5411-0410-84c9-993ea22221af>
2002-11-28 08:40:45 +0000

    bugfixes in classpath definition
    git-svn-id: https://xmlresume.svn.sourceforge.net/svnroot/xmlresume@533 58bf4eea-5411-0410-84c9-993ea22221af

--- trunk/resume-website/src/www/orc/convert.sh
+++ trunk/resume-website/src/www/orc/convert.sh
@@ -15,29 +15,23 @@
 
 # You may need to change the following variables:
 SUPPORT_HOME="@SUPPORT_HOME@"
-ANTCMD="${SUPPORT_HOME}/ant/ant"
 JAVACMD=java
 MD5CMD=md5sum
 
-# Load user-specific configuration
-if [ -f "${HOME}/.antrc" ]; then 
-	. "${HOME}/.antrc"
-fi
-
-
 # Ant's ClassLoader is... frail.  We set the classpath
 # outside of Ant, save ourselves some heartache.
-cp="${SUPPORT_HOME}/fop.jar"
+cp="/usr/java/j2sdk1.4.0_01/lib/tools.jar"
 for jarfile in `ls -1 ${SUPPORT_HOME} | grep .jar`; do
 	cp="${cp}:${SUPPORT_HOME}/${jarfile}"
 done
 for jarfile in `ls -1 ${SUPPORT_HOME}/ant/lib | grep .jar`; do
-	cp="${cp}:${SUPPORT_HOME}/${jarfile}"
+	cp="${cp}:${SUPPORT_HOME}/ant/lib/${jarfile}"
 done
 export CLASSPATH=$cp
 
 # Add option for the CLASSPATH
-ANT_OPTS="${ANT_OPTS} -classpath ${LOCALCLASSPATH}"
+ANT_OPTS="${ANT_OPTS} -cp ${CLASSPATH}"
+ANT_OPTS="${ANT_OPTS} -Dant.home=${SUPPORT_HOME}/ant"
 
 cd @WWW_ROOT_FS@/orc/incoming
 for resume in `ls -1 | grep '^orc'`; 
@@ -47,7 +41,8 @@ do
       touch .CONVERT.$resume
 	cd $resume
 	grep '^email =' user.props | cut -f 3 -d" " | ${MD5CMD} >> ../../users.md5
-	${ANTCMD} -verbose -propertyfile user.props \
+	${JAVACMD} ${ANT_OPTS} org.apache.tools.ant.Main \
+	-verbose -propertyfile user.props \
 	-find build.xml dispatch >& ./out/antlog.txt
 #	sendmail -f'noreply@xmlresume.sourceforge.net' -t < reply.email
 	cd ..
